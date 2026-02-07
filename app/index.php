d6287b17704dbd4f1c3388a779710569d046ada9
CRICSPORTCENTRAL <madhavdagur97@gmail.com>
2026-02-07 14:39:04 +0530

    Create index.php for DASH manifest output
    Add PHP script to output DASH manifest with ClearKey DRM

---
+++ app/index.php
@@ -0,0 +1,10 @@
+<?php
+// Tells the player to treat this as raw text
+header('Content-Type: text/plain');
+
+$manifest = "https://livem-d-01-hub-we.akamaized.net/variant/v1/dai-event-prd-pal-west/vcg-01-d/DASH_DASH/Live/channel(vcg-01-ch-hd-02)/manifest.mpd?vcfilter=5f8c15b4-5bac-41cf-b227-3685f47e6160";
+$clearkey = "ae7076b647133606b704d33e4cef5134:810251515584cf71216d9a2d1ebf04bb";
+
+// The "Pipe" format for NS Player
+echo $manifest . "|drmScheme=clearkey&drmLicense=" . $clearkey;
+
